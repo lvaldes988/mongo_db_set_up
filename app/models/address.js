d715f94e501c37cfc3ace7c6f28d7095865ca01d
liset valdes <lvaldes@techlaunch.io>
2018-04-01 15:26:11 -0400

    first commit

---
+++ app/models/address.js
@@ -0,0 +1,18 @@
+//load the thisng we need
+
+var mongoose = requiere ('mongoose');
+
+//to define the sqchema for our user model in our db table
+var addressSchema = mongoose.Schema({
+    name: String,
+    street_1: String,
+    street_2: String, 
+    city: String, 
+    state: String, 
+    zip_code: String, 
+    country: String, 
+    user: String,
+});
+
+//to create the model and expose in to our app
+module.export = mongoose.model('address', addressSchema); 
---
+++ app/models/user.js
@@ -0,0 +1,37 @@
+import { Schema } from 'mongoose';
+
+//load the things we need
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+//defien the schema for our user model
+var userSchema = mongoose.Schema({
+    email: {type: String, lowercase:true, trim: true}, 
+    password: String, 
+    name: {type: String, trim: true}, 
+    emailConfirmationToken: String, 
+    resetPasswordToken: String, 
+    resetPasswordTokenExpires: Number
+});
+
+//generate a hash so our db never stror the real password
+//instead it gets converted to a hash table for security
+
+userSchema.methods.generateHash = function (password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+//checking if password is valid
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+//checking email confirmation
+userSchema.methods.isEmailConfirmed = function (){
+    return this.emailConfirmed;
+};
+
+//create the model and expose to our app
+module.exports = mongoose.model('user', userSchema);
+
+
