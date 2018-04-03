858fd06fe06fd9067f4d06f018b9658d18c29b73
liset valdes <lvaldes@techlaunch.io>
2018-04-03 18:05:55 -0400

    other commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = function(){
-    var mongoose = require('mongoose');
-    var mongo = mongoose.connect('mongodb://localhost/address_book');
-    return mongo;
-  }
+  var mongoose = require('mongoose');
+  var mongo = mongoose.connect('mongodb://localhost/address_book');
+  return mongo;
+}
